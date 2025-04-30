6b07736f80d2270fbc247a59cede3ff76bd4b378
Abdessamad Himi <abhimi@e2r10p4.1337.ma>
2025-04-30 13:00:52 +0100

    implement echo

--- echo.c
+++ echo.c
@@ -6,10 +6,65 @@
 /*   By: abhimi <abhimi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 14:44:53 by abhimi            #+#    #+#             */
-/*   Updated: 2025/04/29 14:45:40 by abhimi           ###   ########.fr       */
+/*   Updated: 2025/04/30 12:59:43 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+//#include "minishell.h"
 
-//will implement a soon
+#include <stdio.h>
+
+void    ft_echo(char **arg)
+{
+    int i;
+
+    int flag;
+    if (!arg[1])
+    {
+        printf("\n");
+        return ;
+    }
+    if (arg[1][0] == '-' && arg[1][1] == 'n' && arg[1][2] == '\0')
+    {
+            flag = 1;
+    }
+    else
+        flag = 0;
+    if (flag)
+    {
+        i = 2;
+        while (arg[i])
+        {
+            if (!arg[i + 1])
+            {
+                printf("%s",arg[i]);
+                break;
+            }
+            else
+                printf("%s ",arg[i]);
+            i++;
+        }
+    }
+    else
+    {
+        i = 1;
+        while (arg[i])
+        {
+            if(!arg[i +1])
+            {    printf("%s\n",arg[i]);
+                break;
+            }
+            else
+                printf("%s ",arg[i]);
+            i++;
+        }
+    }
+}
+
+int main(int ac, char **arg)
+{
+    (void)ac;
+    
+    ft_echo(arg);
+    return 0;
+}
