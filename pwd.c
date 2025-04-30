b1edae5b7871087ac1eaa119ec9e6592b66eb7f6
Abdessamad Himi <abhimi@e2r10p4.1337.ma>
2025-04-30 12:25:18 +0100

    implement pwd

--- pwd.c
+++ pwd.c
@@ -6,10 +6,45 @@
 /*   By: abhimi <abhimi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 14:50:39 by abhimi            #+#    #+#             */
-/*   Updated: 2025/04/29 14:51:13 by abhimi           ###   ########.fr       */
+/*   Updated: 2025/04/30 12:24:07 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+//#include "minishell.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-// will implement a soon
+void ft_pwd(char **args)
+{
+    int i;
+    char *path;
+
+    i = 1;
+    
+    while(args[i])
+    {
+        if (args[i][0] != ' ' && args[i][0] != '\0')
+        {
+            printf("pwd : too many arguments\n");
+            return ;
+        }
+        i++;
+    }
+    path = getcwd(NULL, 0);
+    if(!path)
+    {
+        printf("pwd: getcwd failed\n");
+        free(path);
+        return ;
+    }
+    printf("%s\n", path);
+    free(path);
+}
+
+int main(int ac,char **arg)
+{
+    (void) ac;
+    ft_pwd(arg);
+    return 0;
+}
