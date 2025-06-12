e082aaac8c86688a177dfa1ec41d7a1942f57c95
Abdessamad Himi <abhimi@e2r10p4.1337.ma>
2025-06-12 15:53:35 +0100

    fix exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,11 +6,34 @@
 /*   By: abhimi <abhimi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 14:54:16 by abhimi            #+#    #+#             */
-/*   Updated: 2025/06/12 14:08:50 by abhimi           ###   ########.fr       */
+/*   Updated: 2025/06/12 15:49:33 by abhimi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+
+int ft_checknum_exit(char *str)
+{
+    int  i;
+
+    i = 0;
+    while (str[i] == ' ')
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+        i++;
+    while (str[i] >= '0' && str[i] <= '9')
+        i++;
+    if (!ft_isdigit(str[i - 1]) && str[i] != ' ' && str[i])
+        return (0);
+    while (str[i] == ' ')
+        i++;
+    if (str[i] == '\0')
+        return (1);
+    else
+        return(0);
+    
+}
 
 int ft_exit(char **arg, int last_status)
 {
@@ -19,29 +42,20 @@ int ft_exit(char **arg, int last_status)
     i = 0;
     while (arg[i])
         i++;
-    if (i > 1)
+    if (i > 2)
     {
         ft_putstr_fd(" too many arguments\n", 2);
         exit (1);
     }
-    i = 0;
     if (arg[1])
     {
-        while (arg[1][i])
+        if (!ft_checknum_exit(arg[1]))
         {
-            if (arg[1][i] == '+' || arg[1][i] == '-' )
-            {
-                i++;
-                continue;
-            }
-            if (!ft_isdigit(arg[1][i]))
-            {
-                ft_putstr_fd(" numeric argument required\n", 2);
-                exit (2);
-            }
-            i++;
+            ft_putstr_fd(" numeric argument required\n", 2);
+            exit (2);
         }
         exit(ft_atoi(arg[1]));
     }
+    printf("exit\n");
     exit(last_status);
 }
